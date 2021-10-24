6a7f924156c132d15208df44cf128285a715f761
Mat Hare <mat_hare@yahoo.com>
2021-10-24 15:21:14 +0100

    Refactor API request method calls

--- src/test/java/com/typicode/jsonplaceholder/steps/CommonSteps.java
+++ src/test/java/com/typicode/jsonplaceholder/steps/CommonSteps.java
@@ -1,10 +1,11 @@
 package com.typicode.jsonplaceholder.steps;
 
-import com.typicode.jsonplaceholder.helpers.RequestHelpers;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,23 +15,32 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class CommonSteps {
 
-    public static Response response;
-    public static List<Response> responses;
+    private RequestSpecification request;
+    private Response response;
+    private List<Response> responses;
 
     @Before
-    public static void setup() {
+    public void setup() {
+        RestAssured.baseURI = "https://wordsapiv1.p.rapidapi.com/words/";
+        request = RestAssured.given();
+        request.header("x-rapidapi-key", System.getProperty("API_KEY"));
+        request.header("Content-Type", "application/json");
         responses = new ArrayList<>();
     }
 
     @When("^I make a GET request to the \"(.+)\" endpoint for the word \"(.+)\"$")
-    public static void makeRequest(String endpoint, String word) {
-        endpoint = endpoint.toLowerCase().replace(" ", "");
-        response = RequestHelpers.sendGetRequest(endpoint, word);
+    public void makeRequest(String endpoint, String word) {
+        this.endpoint = endpoint;
+        this.word = word;
+        response = request.get(buildRequestURI(word, endpoint));
         responses.add(response);
     }
 
     @Then("the response has a status code of {int}")
-    public static void verifyResponseStatusCode(int code) {
+    public void verifyResponseStatusCode(int code) {
         assertThat(response.getStatusCode(), equalTo(code));
+    }
+    private String buildRequestURI(String word, String endpoint) {
+        return (word + "/" + endpoint.replace(" ", "")).toLowerCase();
     }
 }
