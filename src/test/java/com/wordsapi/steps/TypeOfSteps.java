59ee577865638de689bee1dd08e75c46ba81cf47
Mat Hare <mat_hare@yahoo.com>
2021-11-07 12:23:28 +0000

    Extract Json field to constant

--- src/test/java/com/wordsapi/steps/TypeOfSteps.java
+++ src/test/java/com/wordsapi/steps/TypeOfSteps.java
@@ -10,14 +10,15 @@ import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 
 public class TypeOfSteps {
+    private static final String FIELD = "typeOf";
 
     @Then("the word is an example of the following types")
     public void verifyAgainstDataTable(DataTable dataTable) {
-        assertThat(JsonPath.from(response.asString()).get("typeOf"), equalTo(dataTable.asList()));
+        assertThat(JsonPath.from(response.asString()).get(FIELD), equalTo(dataTable.asList()));
     }
 
     @Then("the word is not a \"type of\" anything")
     public void verifyTypeOfArrayEmpty() {
-        assertThat(JsonPath.from(response.asString()).get("typeOf"), empty());
+        assertThat(JsonPath.from(response.asString()).get(FIELD), empty());
     }
 }
